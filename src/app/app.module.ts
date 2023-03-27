51ed719135b62a1be12bc4eea302a3a99fcdd518
Agustin Rios <agrios@frba.utn.edu.ar>
2023-03-27 17:26:44 -0300

    se agregaron componentes y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    ExperienciaComponent,
+    HardAndSoftSkillsComponent
   ],
   imports: [
     BrowserModule,
